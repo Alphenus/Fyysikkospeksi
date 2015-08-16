cd0c0e56ddbc10e6671b6c7c6f55db4e63020eb8
Alphenus <alpi@alpiini.com>
2015-08-16 21:19:38 +0300

    Paljon muutoksia

--- js/app.js
+++ js/app.js
@@ -12,3 +12,18 @@ $(document).foundation({
   offset_by_height: true // whether to offset the destination by the expedition height. Usually you want this to be true, unless your expedition is on the side.
 }
 });
+
+jQuery(document).ready(function($){
+     $('a[href^="#"]').on('click',function (e) {
+         e.preventDefault();
+
+         var target = this.hash,
+         $target = $(target);
+
+         $('html, body').stop().animate({
+             'scrollTop': $target.offset().top-80 //TÄMÄ lukuarvo top:n jälkeen on offset!
+         }, 900, 'swing', function () {
+             window.location.hash = target;
+         });
+     });
+});
